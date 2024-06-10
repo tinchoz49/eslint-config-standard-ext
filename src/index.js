261b4342d034ba989127e356b6f7fb9e16518d5b
Martin Acosta <tinchoz49@gmail.com>
2024-06-10 01:35:38 -0300

    fix ts/typle-slash-reference

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ import { isPackageExists } from 'local-pkg'
 import fixFormatters from './fix-formatters.js'
 import javascriptStandardRules from './javascript-standard-rules.js'
 import stylisticOverrides from './stylistic-overrides.js'
-import typescriptStandardRules, { typescriptTypeAwareRules } from './typescript-standard-rules.js'
+import typescriptStandardRules from './typescript-standard-rules.js'
 
 /**
  * @param {OptionsConfig & { javascript?: OptionsIsInEditor & OptionsOverrides & { organizeImports?: boolean } }} options
@@ -70,61 +70,11 @@ export function standard(options = {}, ...userConfigs) {
     },
     typescript,
   }, ...userConfigs)
-    .override('antfu/javascript/rules', javascriptStandardRules(options?.javascript?.organizeImports))
-    .override('antfu/jsdoc/rules', {
-      rules: {
-        'jsdoc/require-returns-check': 'off',
-        'jsdoc/require-returns-description': 'off',
-      },
-    })
-    .override('antfu/test/rules', {
-      rules: {
-        'test/no-import-node-test': 'off',
-        'test/no-only-tests': 'error',
-      },
-    })
-    .override('antfu/imports/rules', {
-      rules: {
-        'import/export': ['error'],
-        'import/extensions': 'off',
-        'import/first': ['error'],
-        'import/no-absolute-path': ['error', { amd: false, commonjs: true, esmodule: true }],
-        'import/no-duplicates': ['error'],
-        'import/no-named-default': ['error'],
-        'import/no-webpack-loader-syntax': ['error'],
-        'import/order': 'off',
-      },
-    })
-    .override('antfu/node/rules', {
-      rules: {
-        'node/prefer-global/buffer': 'off',
-        'node/prefer-global/process': 'off',
-      },
-    })
+
+  javascriptStandardRules(config, options?.javascript?.organizeImports)
 
   if (typescript) {
-    config.override('antfu/typescript/rules', typescriptStandardRules())
-
-    if ('tsconfigPath' in resolveSubOptions(options, 'typescript')) {
-      config.override('antfu/typescript/rules-type-aware', typescriptTypeAwareRules())
-    }
-
-    config
-      .override('antfu/typescript/disables/dts', {
-        rules: {
-          'ts/triple-slash-reference': 'off',
-        },
-      })
-      .append({
-        files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
-        name: 'typescript/disables/javascript',
-        rules: {
-          'ts/explicit-function-return-type': 'off',
-          'ts/explicit-member-accessibility': 'off',
-          'ts/explicit-module-boundary-types': 'off',
-          'ts/triple-slash-reference': 'off',
-        },
-      })
+    typescriptStandardRules(config, 'tsconfigPath' in resolveSubOptions(options, 'typescript'))
   }
 
   if (formattersOptions) {
--- src/typescript-standard-rules.js
+++ src/typescript-standard-rules.js
@@ -1,11 +1,16 @@
 /* eslint perfectionist/sort-objects: "error" */
 
 /** @typedef {import('@antfu/eslint-config').TypedFlatConfigItem} TypedFlatConfigItem */
+/** @typedef {import('@antfu/eslint-config').ConfigNames} ConfigNames */
+/**
+ * @template T, E
+ * @typedef {import('eslint-flat-config-utils').FlatConfigComposer} FlatConfigComposer
+ */
 
 /**
  * @returns {(config: TypedFlatConfigItem) => Promise<TypedFlatConfigItem>}
  */
-export function typescriptTypeAwareRules() {
+function typescriptTypeAwareRules() {
   return async (config) => {
     return {
       ...config,
@@ -47,144 +52,171 @@ export function typescriptTypeAwareRules() {
 }
 
 /**
- * @returns {(config: TypedFlatConfigItem) => Promise<TypedFlatConfigItem>}
+ * @param {FlatConfigComposer<TypedFlatConfigItem, ConfigNames>} config
+ * @param {boolean} isTypeAware
  */
-export default function typescriptStandardRules() {
-  return async (config) => {
-    return {
-      ...config,
-      rules: {
-        'no-dupe-class-members': 'off',
-        'no-loss-of-precision': 'off',
-        'no-redeclare': 'off',
-        'no-use-before-define': 'off',
-        'no-useless-constructor': 'off',
-        'ts/adjacent-overload-signatures': ['error'],
-        'ts/array-type': ['error', { default: 'array-simple' }],
-        'ts/ban-ts-comment': ['error', {
-          'minimumDescriptionLength': 3,
-          'ts-check': false,
-          'ts-expect-error': 'allow-with-description',
-          'ts-ignore': true,
-          'ts-nocheck': true,
-        }],
-        'ts/ban-tslint-comment': ['error'],
-        'ts/ban-types': ['error', {
-          extendDefaults: false,
-          types: {
-            '{}': {
-              message: [
-                '`{}` actually means "any non-nullish value".',
-                '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
-                '- If you want a type meaning "any value", you probably want `unknown` instead.',
-              ].join('\n'),
-            },
-            'BigInt': {
-              fixWith: 'bigint',
-              message: 'Use bigint instead',
-            },
-            'Boolean': {
-              fixWith: 'boolean',
-              message: 'Use boolean instead',
-            },
-            'Function': {
-              message: [
-                'The `Function` type accepts any function-like value.',
-                'It provides no type safety when calling the function, which can be a common source of bugs.',
-                'It also accepts things like class declarations, which will throw at runtime as they will not be called with `new`.',
-                'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.',
-              ].join('\n'),
-            },
-            'Number': {
-              fixWith: 'number',
-              message: 'Use number instead',
-            },
-            // object typing
-            'Object': {
-              message: [
-                'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
-                '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
-                '- If you want a type meaning "any value", you probably want `unknown` instead.',
-              ].join('\n'),
-            },
-            'String': {
-              fixWith: 'string',
-              message: 'Use string instead',
-            },
-            'Symbol': {
-              fixWith: 'symbol',
-              message: 'Use symbol instead',
-            },
-          },
-        }],
-        'ts/class-literal-property-style': ['error', 'fields'],
-        'ts/consistent-generic-constructors': ['error', 'constructor'],
-        'ts/consistent-indexed-object-style': ['error', 'record'],
-        'ts/consistent-type-assertions': [
-          'error',
-          {
-            assertionStyle: 'as',
-            objectLiteralTypeAssertions: 'never',
-          },
-        ],
-        'ts/consistent-type-definitions': ['error', 'interface'],
-        'ts/consistent-type-imports': ['error', {
-          disallowTypeAnnotations: true,
-          fixStyle: 'inline-type-imports',
-          prefer: 'type-imports',
-        }],
-        'ts/explicit-function-return-type': ['error', {
-          allowDirectConstAssertionInArrowFunctions: true,
-          allowExpressions: true,
-          allowHigherOrderFunctions: true,
-          allowTypedFunctionExpressions: true,
-        }],
-        'ts/method-signature-style': ['error'],
-        'ts/naming-convention': ['error', {
-          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-          leadingUnderscore: 'allow',
-          selector: 'variableLike',
-          trailingUnderscore: 'allow',
-        }],
-        'ts/no-array-constructor': ['error'],
-        'ts/no-dupe-class-members': 'error',
-        'ts/no-dynamic-delete': ['error'],
-        'ts/no-empty-interface': ['error', { allowSingleExtends: true }],
-        'ts/no-explicit-any': 'off',
-        'ts/no-extra-non-null-assertion': ['error'],
-        'ts/no-extraneous-class': ['error', { allowWithDecorator: true }],
-        'ts/no-import-type-side-effects': 'off',
-        'ts/no-invalid-void-type': ['error'],
-        'ts/no-loss-of-precision': ['error'],
-        'ts/no-misused-new': ['error'],
-        'ts/no-namespace': ['error'],
-        'ts/no-non-null-asserted-optional-chain': ['error'],
-        'ts/no-non-null-assertion': ['error'],
-        'ts/no-redeclare': ['error', { builtinGlobals: false }],
-        'ts/no-require-imports': 'off',
-        'ts/no-this-alias': ['error', { allowDestructuring: true }],
-        'ts/no-unnecessary-type-constraint': ['error'],
-        'ts/no-unused-expressions': ['error', {
-          allowShortCircuit: true,
-          allowTaggedTemplates: true,
-          allowTernary: true,
-          enforceForJSX: false,
-        }],
-        'ts/no-unused-vars': 'off',
-        'ts/no-use-before-define': ['error', {
-          classes: false,
-          enums: false,
-          functions: false,
-          typedefs: false,
-          variables: false,
-        }],
-        'ts/no-useless-constructor': ['error'],
-        'ts/no-var-requires': ['error'],
-        'ts/prefer-function-type': ['error'],
-        'ts/prefer-ts-expect-error': ['error'],
-        'ts/triple-slash-reference': ['error', { lib: 'never', path: 'never', types: 'never' }],
-        'ts/unified-signatures': 'off',
-      },
-    }
+export default function typescriptStandardRules(config, isTypeAware) {
+  if (isTypeAware) {
+    config.override('antfu/typescript/rules-type-aware', typescriptTypeAwareRules())
   }
+
+  config
+    .override('antfu/typescript/disables/dts', {
+      rules: {
+        'ts/triple-slash-reference': 'off',
+      },
+    })
+    .append({
+      files: ['**/*.d.ts'],
+      rules: {
+        'ts/triple-slash-reference': 'off',
+      },
+    })
+    .append({
+      files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
+      name: 'typescript/disables/javascript',
+      rules: {
+        'ts/explicit-function-return-type': 'off',
+        'ts/explicit-member-accessibility': 'off',
+        'ts/explicit-module-boundary-types': 'off',
+        'ts/triple-slash-reference': 'off',
+      },
+    })
+    .override('antfu/typescript/rules', async (config) => {
+      return {
+        ...config,
+        rules: {
+          'no-dupe-class-members': 'off',
+          'no-loss-of-precision': 'off',
+          'no-redeclare': 'off',
+          'no-use-before-define': 'off',
+          'no-useless-constructor': 'off',
+          'ts/adjacent-overload-signatures': ['error'],
+          'ts/array-type': ['error', { default: 'array-simple' }],
+          'ts/ban-ts-comment': ['error', {
+            'minimumDescriptionLength': 3,
+            'ts-check': false,
+            'ts-expect-error': 'allow-with-description',
+            'ts-ignore': true,
+            'ts-nocheck': true,
+          }],
+          'ts/ban-tslint-comment': ['error'],
+          'ts/ban-types': ['error', {
+            extendDefaults: false,
+            types: {
+              '{}': {
+                message: [
+                  '`{}` actually means "any non-nullish value".',
+                  '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+                  '- If you want a type meaning "any value", you probably want `unknown` instead.',
+                ].join('\n'),
+              },
+              'BigInt': {
+                fixWith: 'bigint',
+                message: 'Use bigint instead',
+              },
+              'Boolean': {
+                fixWith: 'boolean',
+                message: 'Use boolean instead',
+              },
+              'Function': {
+                message: [
+                  'The `Function` type accepts any function-like value.',
+                  'It provides no type safety when calling the function, which can be a common source of bugs.',
+                  'It also accepts things like class declarations, which will throw at runtime as they will not be called with `new`.',
+                  'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.',
+                ].join('\n'),
+              },
+              'Number': {
+                fixWith: 'number',
+                message: 'Use number instead',
+              },
+              // object typing
+              'Object': {
+                message: [
+                  'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
+                  '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+                  '- If you want a type meaning "any value", you probably want `unknown` instead.',
+                ].join('\n'),
+              },
+              'String': {
+                fixWith: 'string',
+                message: 'Use string instead',
+              },
+              'Symbol': {
+                fixWith: 'symbol',
+                message: 'Use symbol instead',
+              },
+            },
+          }],
+          'ts/class-literal-property-style': ['error', 'fields'],
+          'ts/consistent-generic-constructors': ['error', 'constructor'],
+          'ts/consistent-indexed-object-style': ['error', 'record'],
+          'ts/consistent-type-assertions': [
+            'error',
+            {
+              assertionStyle: 'as',
+              objectLiteralTypeAssertions: 'never',
+            },
+          ],
+          'ts/consistent-type-definitions': ['error', 'interface'],
+          'ts/consistent-type-imports': ['error', {
+            disallowTypeAnnotations: true,
+            fixStyle: 'inline-type-imports',
+            prefer: 'type-imports',
+          }],
+          'ts/explicit-function-return-type': ['error', {
+            allowDirectConstAssertionInArrowFunctions: true,
+            allowExpressions: true,
+            allowHigherOrderFunctions: true,
+            allowTypedFunctionExpressions: true,
+          }],
+          'ts/method-signature-style': ['error'],
+          'ts/naming-convention': ['error', {
+            format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+            leadingUnderscore: 'allow',
+            selector: 'variableLike',
+            trailingUnderscore: 'allow',
+          }],
+          'ts/no-array-constructor': ['error'],
+          'ts/no-dupe-class-members': 'error',
+          'ts/no-dynamic-delete': ['error'],
+          'ts/no-empty-interface': ['error', { allowSingleExtends: true }],
+          'ts/no-explicit-any': 'off',
+          'ts/no-extra-non-null-assertion': ['error'],
+          'ts/no-extraneous-class': ['error', { allowWithDecorator: true }],
+          'ts/no-import-type-side-effects': 'off',
+          'ts/no-invalid-void-type': ['error'],
+          'ts/no-loss-of-precision': ['error'],
+          'ts/no-misused-new': ['error'],
+          'ts/no-namespace': ['error'],
+          'ts/no-non-null-asserted-optional-chain': ['error'],
+          'ts/no-non-null-assertion': ['error'],
+          'ts/no-redeclare': ['error', { builtinGlobals: false }],
+          'ts/no-require-imports': 'off',
+          'ts/no-this-alias': ['error', { allowDestructuring: true }],
+          'ts/no-unnecessary-type-constraint': ['error'],
+          'ts/no-unused-expressions': ['error', {
+            allowShortCircuit: true,
+            allowTaggedTemplates: true,
+            allowTernary: true,
+            enforceForJSX: false,
+          }],
+          'ts/no-unused-vars': 'off',
+          'ts/no-use-before-define': ['error', {
+            classes: false,
+            enums: false,
+            functions: false,
+            typedefs: false,
+            variables: false,
+          }],
+          'ts/no-useless-constructor': ['error'],
+          'ts/no-var-requires': ['error'],
+          'ts/prefer-function-type': ['error'],
+          'ts/prefer-ts-expect-error': ['error'],
+          'ts/triple-slash-reference': ['error', { lib: 'never', path: 'never', types: 'never' }],
+          'ts/unified-signatures': 'off',
+        },
+      }
+    })
 }
